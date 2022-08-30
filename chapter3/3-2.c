2a4d27782d1a7b3274ba87a4db886edd143b3b83
Leon <liang00716@gmail.com>
2022-08-30 09:34:12 +0800

    add chapter3 folder

---
+++ chapter3/3-2.c
@@ -0,0 +1,15 @@
+//²Ä¤T³¹²ßÃD 2 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 12400;
+	double num2 = 5.234;
+	
+	printf("%d is an integer\n", num1 * num1);
+	printf("%f is a double\n", num2 * num2);
+	
+	system("pause");
+	return 0;
+} 
